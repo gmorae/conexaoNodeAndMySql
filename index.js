4631f9e86daa58cbc77c31a3a4a67b93925a340d
gmorae <gmmartins06@gmail.com>
2019-11-02 04:13:40 -0300

    password database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('barbearia', 'root', '', {
+const sequelize = new Sequelize('barbearia', 'root', '123456', {
     host: "localhost",
     dialect: 'mysql',
     port: '5050'
